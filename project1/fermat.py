67a53d62e8c2f4c0a8be1943afbb376480ba5db9
seanvsmith1901 <168485841+seanvsmith1901@users.noreply.github.com>
2024-09-17 22:24:15 -0600

    bleh
    finish project 1

--- project1/fermat.py
+++ project1/fermat.py
@@ -11,38 +11,38 @@ def prime_test(N: int, k: int) -> tuple[str, str]:
 
 # You will need to implement this function and change the return value.
 # Takes in x and N, and an integer component y, and returns x^y mod N
-def mod_exp(x: int, y: int, N: int) -> int:  # time complexity is O(n^3)
+def mod_exp(x: int, y: int, N: int) -> int:  # time complexity is O(n^3) in terms of bits and O(log(n))
     if y == 0: # constant time
         return 1
-    z = mod_exp(x, (y//2), N) # recursive call   # USE INTEGER DIVISION NOT FLOOR
-    if (y % 2) == 0: # constant time # if even
-        return (z*z) % N # assuming modulo is constant time
+    z = mod_exp(x, (y//2), N)  # USE INTEGER DIVISION NOT FLOOR
+    if (y % 2) == 0:  # if even
+        return (z*z) % N
     else:  # if odd
-        return (x * (z*z)) % N #
+        return (x * (z*z)) % N
 
 
 # You will need to implement this function and change the return value.
 def fprobability(k: int) -> float: # never tested but pretty sure
-    return (1/(pow(2, k)))  # constant time
+    return (1/(pow(2, k)))
 
 
 # You will need to implement this function and change the return value.
 def mprobability(k: int) -> float:  # never tested but pretty sure
-    return (3/pow(4, k))  # constant time, if pow is considered O(1)
+    return (3/pow(4, k))
 
 
 # fermats test for primes, given prime number N, and the number of iterations run (K)
-def fermat(N: int, k: int) -> str: # time complexity is kO(N-1)^2
-    for i in range(k): # repeat K times
+def fermat(N: int, k: int) -> str:  # should have a running time of O(n^4)
+    for i in range(k):
         a = random.randint(1, N-1) # pick a rand int (constant time)
-        if (mod_exp(a, N-1, N)) != 1: # however long modular exponentiation takes
+        if (mod_exp(a, N-1, N)) != 1: # run mod_exp (o(n^3)
             return "composite"
     return "prime"
 
 
 # miller rabbin test for primes given a number N and the number of iterations to run K
 def miller_rabin(N: int, k: int) -> str: # woohoo! I am a genius! I got it working!
-    for i in range(k): # run K times # best case is O(1), worst case is O((n-1)^2)
+    for i in range(k):
         a = random.randint(1, N-1)  # pick a random number
         result = mod_exp(a, N-1, N)  # calculate the result with modular exponentiation
         if result == 1:  # if we start with a 1 we have to keep checking down the list
@@ -52,7 +52,7 @@ def miller_rabin(N: int, k: int) -> str: # woohoo! I am a genius! I got it worki
             return "composite"
     return "prime"  # if we never return composite its prolly prime
 
-# pretty sure that this fucntion is Olog(n)^2, becuase mod_exp is Olog(n) and this functino is also Olog(N)
+
 def miller_rabin_help(a, exp, mod): # a is our term, N is our exponent and mod is our %
     result = mod_exp(a, exp, mod) # calculate the result
     if result == mod-1:  # mod -1, we can break cause its prime
@@ -83,3 +83,43 @@ if __name__ == '__main__':
     parser.add_argument('k', type=int)
     args = parser.parse_args()
     main(args.number, args.k)
+
+
+
+# WRITE UP: SPACE AND TIME COMPLEXITIES OF RELATED FUNCTIONS
+# if you would like to see this in a better format, here's the drive page:
+# https://docs.google.com/document/d/15ysNv9G759zuxsQsH30g152rZIbs5SVbS4m-p-GV5tQ/edit?usp=sharing
+#
+#
+# mod_exp: We are sort of making the assumption that n = log(2)x = log(2)y = log(2)N.
+# The rest is fairly simple - we have a constant for the return statement, we have a recursive call that goes off log(N) times, and then we have 2 return statements, which are both n2 return time.
+# Given our n * n * log(2)N (we can simplify log(2)N to n) for space complexity we have n * n * n or O(n3)
+# For our space complexity, we aren’t creating any lists or anything, so the only non-constant affecting memory is our recursive call, which we do log(2)N times, or n.
+# So our space complexity is O(n)
+#
+#
+# Fermat: we first have to do this K times, so K will be in there somewhere.
+# So we will have K times picking our randINt, we can assume to be constant time.
+# We already know that our mod_exp has a time of O(n3), so our total time complexity will be
+# O(n3*k), and if we can assume that K and N are similar sizes, then we can say that we are dealing with kO(n3) (this can either be rewritten as O(n^3) assuming K is a constant, or we can rewrite it as O(n^4) if we assume that K is similar to N. for the duration of this paper, I will be assuming that K is constant and be using O(n^3)
+# For space complexity, we are storing the var a but then deleting it, and we don’t appear to do anything else with it. We just call K times on our mod_exp, but everytime we call K we overwrite the old var. Therefore, I am pretty sure this has the same space complexity as mod_exp, which is to say, O(n)
+#
+#
+# Miller Rabbin: so we once again have to do this K times, so K will be in there somewhere.
+# We pick a randit, which is constant time, and then we do mod_exp. Mod_exp is O(n^3).
+# If it's not prime (assume worst case) we then have to run the help function on top of that.
+# We then get to this helper function.
+# And under miller rabbin help, we run mod_exp again, which has a time of O(n3), we then check, if it fails (assume worst case) then we run it within itself and we run it a max of log(exp) times, which simplifies out to n, so its total time is n(n^3) or n^4.
+# n^3 + n^4 or n^4 complexity, where n^3 represents the mod_exp call outside helper
+# n^4 represents the n^3 mod_exp call inside the helper, and we call the helper log(N) or n. Because K is a constant we can multiply it.
+# Total time complexity: kO(n4) (I am going to assume K is constant and use O(n^4) for the rest of this paper)
+# Space complexity:  the only thing here that takes space is the recursive call to its help function, as well as the call to mod_exp. Miller rabbin will call itself log(exp) times or O(n) times, and mod_exp calls itself log(y) or n times. Either way, we are dealing with O(n) space, where n is either log(y) or log(exp).
+#
+#
+# probability: The space complexity on this is O(1) lets get that right out the way.
+# The time complexity is a little harder to grasp - given that division has a call time of O(n^2), we have O(n^2) multiplied by 2^n. Given my old rules, I do believe that an dominates over na, so we can have this as  O(n^2)
+#
+#
+# mpProbability: This one is very similar to fProbability, where
+# Time complexity: O(n3) (see the process above but replace 2 with 3). We can simplify out the 3 as it is a constant and we want the shortest order possible.
+# Space complexity: O(1)
--- project1/rsa.py
+++ project1/rsa.py
@@ -1,6 +1,7 @@
 import random
 import sys
 
+import fermat
 # This may come in handy...
 from fermat import miller_rabin
 
@@ -19,33 +20,78 @@ primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67
 
 # Implement this function
 def ext_euclid(a: int, b: int) -> tuple[int, int, int]:
-    """
-    The Extended Euclid algorithm
-    Returns x, y , d such that:
-    - d = GCD(a, b)
-    - ax + by = d
+    if b == 0:
+        return (1, 0, a) # a SHOULD be one for extended cases, if a isn't one then we try again
+    (x1, y1, d) = ext_euclid(b, a % b) # magic
+    return (y1, (x1 - ((a//b)*y1)), d) # d is the GCD so we want that
 
-    Note: a must be greater than b
-    """
-    return 0, 0, 0
 
 
 # Implement this function
 def generate_large_prime(bits=512) -> int:
-    """
-    Generate a random prime number with the specified bit length.
-    Use random.getrandbits(bits) to generate a random number of the
-     specified bit length.
-    """
-    return 5  # Guaranteed random prime number obtained through fair dice roll
+    myPrime = 6
+    k = 20
+    while fermat.miller_rabin(myPrime, k) != "prime":
+        myPrime = random.getrandbits(bits)
+
+    return myPrime # Guaranteed random prime number obtained through fair dice roll
 
 
 # Implement this function
 def generate_key_pairs(bits: int) -> tuple[int, int, int]:
-    """
-    Generate RSA public and private key pairs.
-    Return N, e, d
-    - N must be the product of two random prime numbers p and q
-    - e and d must be multiplicative inverses mod (p-1)(q-1)
-    """
-    return 0, 0, 0
+
+    p = generate_large_prime(bits)
+    q = generate_large_prime(bits)
+    N = p * q
+
+    pq = (p-1) * (q-1) # just to save space and make it easier to read
+
+    d = 0
+    index = 0
+    while d != 1:
+        (y1, x1, d) = ext_euclid((p-1) * (q-1), primes[index])
+        index += 1
+
+    e = primes[index-1]  # yeah its funky don't worry about it
+
+    # find D
+
+    (r1, d, r3) = ext_euclid((p-1) * (q-1), e)
+
+    d = d % ((p-1) * (q-1))  # this is VERY imporant, he NEEDS to be positive lol otherwise he will recurse into the floor
+
+
+    print("This is what we are returning ", N, " ", e, " ", d)
+    return (N, e, d)
+
+
+# WRITE UP: SPACE AND TIME COMPLEXITIES OF RELATED FUNCTIONS
+# if you would like to see this in a better format, here's the drive page:
+# https://docs.google.com/document/d/15ysNv9G759zuxsQsH30g152rZIbs5SVbS4m-p-GV5tQ/edit?usp=sharing
+#
+#
+# Ext_euclid: This one has some magic going on, but the biggest thing is its recursive call
+# We know that when we take our a % b, we can’t be any larger than ½ of the size, so we are taking a log of b.
+# We are going to say that a and b are similarly sized inputs, and as such we can say that the big o of this function is O(log(n))
+# In terms of space complexity, the only thing that is happening here is once again, the recursive call. So, our space complexity is going to be the same as our time complexity, which is to say, O(log(n))
+#
+#
+# Generate_Large_Prime: This one has two things going on in it. First, is that I call miller_rabin (I tried both) and based on my last assumption we can say that miller_rabbin is O(n^4).
+# We call this until we get a random number. This is going to require us to use the prime number theorem. Given a random integer from 0-N, the probability of being a prime is approx 1 / ln(N).
+# If we assume that our N is a constant 512 bits, then this reduces to O(1) time. If, however, we assume that this is NOT the case, then we can saw that we will need to run 1 / ln(N) times.
+# This means that we take O(n^4) * 1/ln(n), which still simplifies out to O(n^4) runtime.
+# Time: O(n^4) (given that K is a constant and the prime number theorem)
+# In terms of space, it's actually just O(1) because we are only declaring 4 variables and checking them. Large variables, but never larger than 512 bits. If the bits were not constant, then we would be O(n) for space.
+# Space: O(1) for 512 bits, O(n) for scaling input.
+#
+#
+# Generate_Key_Pairs: this one generates a large prime, which is O(n^4) and does this twice.
+# We then store pq, which is n^2 given p and q, (much slower than checking if its prime)
+# We then calculate d, and if we assume euclid's extent is never larger than Olog(n), we can add that in. This gets us e and we store it. We run ext_euclids again for another O(log(n)) and then make sure d is positive, (O(1)) and then return those pairs.
+# So our big O looks like
+# O(n^4) + n^2 + Olog(n) + O(log(n)) + O(1)
+# So our time complexity is O(n^4), almost entirely just from checking to make sure that our numbers are prime. The rest is easy potatoes.
+# In terms of space complexity, there’s not a lot going on here.
+# The only thing to watch out for here is once again, generating random primes, it can be O(n) when using get_rand_bits(where we have to randomize each bit in N size).
+# Miller rabin test for primality is also O(n) in terms of input size, which is what I use. Ext_euclid is only going to be O(log(n))
+# Space complexity: O(n)
