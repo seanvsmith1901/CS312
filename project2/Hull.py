997e9ea6eb5c276f2f4086ba39b3c6ecc3a0d4e2
seanvsmith1901 <168485841+seanvsmith1901@users.noreply.github.com>
2024-09-26 22:05:39 -0600

    completely changed everytihng
    used the pseudocode from my new session and fixed everything

--- project2/Hull.py
+++ project2/Hull.py
@@ -5,5 +5,22 @@ class Hull: # literally just list of points but I like having it as a class :)
     def __init__(self):
         self.hull = []
 
+    def __init__(self, points_list):
+        for point in points_list:
+            self.__add__(self, point)
+
+    def __init__(self, point):
+        # creates a new hull from the connected points and returns that hull
+        # this is going to be black voodoo magic.
+
+
     def __add__(self, Point):
         self.hull.append(Point)
+
+    def __len__(self):
+        return len(self.hull)
+
+    def __createList__(self): # makes a list of non points (array) to return to finalize stuff
+        # bleh bleh bleh
+        return self
+
--- project2/convex_hull.py
+++ project2/convex_hull.py
@@ -3,56 +3,95 @@
 # from plotting import draw_line, draw_hull, circle_point
 
 import Point
+import Hull
 import random
 
 def compute_hull(points: list[tuple[float, float]]) -> list[tuple[float, float]]:
 
     pointsButAsActualPoints = makePoints(points) # turns our list of points into a point object with a head and whatnot
+    new_hull = Hull(pointsButAsActualPoints)
+    new_hull = create_hull(new_hull)
+    new_list = new_hull.create_list()
+    return new_list
 
+
+def create_hull(hull):
+
+    our_median = median(hull)
     leftList = []
     rightList = []
-    side = "L"
 
-
-
-
-
-    #head = Point # thats not gonna work lol
-    our_median = median(points)
-    print(our_median) # so this should? find the median in terms of its x position
     for point in points:
         if point[0] > our_median[0]:
             leftList.append(point)
         else:
             rightList.append(point)
 
-    if len(list) == 1:
-        new_point = Point(list[0][0], list[0][1], list[0], list[0])
+    if len(leftList) > 3:
+        newHull = Hull(leftList)
+        create_hull(newHull)
 
-    elif len(list) == 2:
-        if side == "L":
-            pass
-            # find rightmost point
-            # make that the head
-        else:
-            pass
-            # find leftmost point
-            # make that the head
+    if len(rightList) > 3:
+        newHull = Hull(leftList)
+        create_hull(newHull)
 
-    elif len(list) == 3:
-        pass # theres a whole LIST of things that need to happen here lol
+    left_hull = formalizeHull(left_list)
+    right_hull = formalizeHull(right_list)
+
+    return joinHull(left_hull, right_hull)
+
+
 
 
 
     return []
 
 
-def median(S): # s is teh list,
+def joinHull(leftHull, rightHull):
+    temp1 = find_upper_tangent(leftHull, rightHull)
+    temp2 = find_lower_tangent(leftHull, rightHull)
+    # do da shimmy
+    # create a new hull from these two and make a hull from that point adn all its pointers
+    return temp1
+
+def formalizeHull(listOfPoints):
+    if len(listOfPoints) == 3:
+        pass
+    if len(listOfPoints) == 2:
+        pass
+    if len(listOfPoints) == 1:
+        pass
 
 
 
+def median(S): # hull!
     # just copying the pseducode given on the slides
     return selection(S, len(S)/2)
+
+
+def selection(S, k):
+
+    Sl = []
+    Sr = []
+    Sv = []
+    # need a random pivot v. there are better ways to pick it. I am not going to do that.
+    v = S[(random.randint(0, len(S)-1))][0]
+
+    # choose a random pivot
+    for i in range(len(S)):
+        if S[i][0] < v: # make sure this is returning again
+            Sl.append(S.at(i))
+        if S[i][0] == v:
+            Sv.append(S.at(i))
+        if S[i][0] > v:
+            Sr.append(S.at(i))
+
+    if k < len(Sl):
+        return selection(Sl, k)
+    if k < len(Sl) + len(Sv):
+        return v
+    if k > len(Sl) + len(Sv):
+        return selection(Sr, k - len(Sl) - len(Sv))
 
 
 def find_upper_tangent(L,R): # returns line L,R where R is now clockwise of L
@@ -96,29 +135,6 @@ def find_lower_tangent(L,R):
     return temp # once again this is the fetcher that we need
 
 
-def selection(S, k):
-    Sl = []
-    Sr = []
-    Sv = []
-    # need a random pivot v. there are better ways to pick it. I am not going to do that.
-    v = S[(random.randint(0, len(S)-1))][0]
-
-
-    # choose a random pivot
-    for i in range(len(S)):
-        if S[i][0] < v:
-            Sl.append(S.at(i))
-        if S[i][0] == v:
-            Sv.append(S.at(i))
-        if S[i][0] > v:
-            Sr.append(S.at(i))
-
-    if k < len(Sl):
-        return selection(Sl, k)
-    if k < len(Sl) + len(Sv):
-        return v
-    if k > len(Sl) + len(Sv):
-        return selection(Sr, k - len(Sl) - len(Sv))
 
 
 def slope(L : Point,R : Point):
