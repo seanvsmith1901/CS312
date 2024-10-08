b620301ea443d95a1a6879cf5b8d556cdbe37763
seanvsmith1901 <168485841+seanvsmith1901@users.noreply.github.com>
2024-10-07 21:53:40 -0600

    made it pass the tests
    it passes the mini little tests now so thats neat

--- Project3/test_priority_queues.py
+++ Project3/test_priority_queues.py
@@ -6,19 +6,19 @@ def test_pqs():
     array_pq = ArrayPQ(items)
     #heap_pq = HeapPQ(items)
 
-    array_pq.set_priority(7, 7)
+    array_pq.setPriority(7, 7)
     #heap_pq.set_priority(7, 7)
 
-    assert next(array_pq) == 7
+    assert array_pq.deleteMin() == 7
     #assert next(heap_pq) == 7
 
-    array_pq.set_priority(3, 3)
-    array_pq.set_priority(5, 1)
+    array_pq.setPriority(3, 3)
+    array_pq.setPriority(5, 1)
     #heap_pq.set_priority(3, 3)
     #heap_pq.set_priority(5, 1)
 
-    assert next(array_pq) == 5
-    assert next(array_pq) == 3
+    assert array_pq.deleteMin() == 5
+    assert array_pq.deleteMin() == 3
 
     #assert next(heap_pq) == 5
     #assert next(heap_pq) == 3
