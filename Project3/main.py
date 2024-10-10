c85856c8d7c074cbc365579326b8b4980d673ac6
seanvsmith1901 <168485841+seanvsmith1901@users.noreply.github.com>
2024-10-09 20:11:17 -0600

    jdjdj
    changes some defaults, force push!

--- Project3/main.py
+++ Project3/main.py
@@ -94,9 +94,9 @@ if __name__ == '__main__':
     # sys.argv = ['main.py', '-n', '10', '--seed', '312', '--density', '0.3', '--noise', '0.05']
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', type=int, help='The number of points to generate', default=50000)
+    parser.add_argument('-n', type=int, help='The number of points to generate', default=5000)
     parser.add_argument('--seed', type=int, default=312, help='Random seed')
-    parser.add_argument('--density', type=float, default=0.0002, help='Fraction of non-inf edges')
+    parser.add_argument('--density', type=float, default=1, help='Fraction of non-inf edges')
     parser.add_argument('--noise', type=float, default=0.01, help='How non-euclidean are the edge weights')
     parser.add_argument('--source', type=int, default=2, help='Starting node')
     parser.add_argument('--target', type=int, default=9, help='Target node')
