fbb6993f719d269ebe180add5b1601c9e5c5547f
seanvsmith1901 <168485841+seanvsmith1901@users.noreply.github.com>
2024-11-18 15:45:03 -0700

    i push
    i push

--- project5/tsp_solve.py
+++ project5/tsp_solve.py
@@ -102,6 +102,11 @@ def greedy_tour(edges: list[list[float]], timer: Timer) -> list[SolutionStats]:
                 continue
 
 
+            if stats and cost > stats[-1].score:
+                n_nodes_pruned += 1
+                cut_tree.cut(tour)
+
+                continue
 
             stats.append(SolutionStats(
                 tour=tour,
@@ -113,8 +118,6 @@ def greedy_tour(edges: list[list[float]], timer: Timer) -> list[SolutionStats]:
                 n_leaves_covered=cut_tree.n_leaves_cut(),
                 fraction_leaves_covered=cut_tree.fraction_leaves_covered()
             ))
-
-            break
 
     if not stats:
         return [SolutionStats(
@@ -131,7 +134,81 @@ def greedy_tour(edges: list[list[float]], timer: Timer) -> list[SolutionStats]:
 
 
 def dfs(edges: list[list[float]], timer: Timer) -> list[SolutionStats]:
-    return []
+
+    stats = []
+    n_nodes_expanded = 0
+    n_nodes_pruned = 0
+    cut_tree = CutTree(len(edges))
+    currentNode = 0  # keep track of where we are
+    # so for the adjacency matrix, go through at that i and then look for the lowest value (that isn't itself) and then store it and consult that node
+    # if the lowest cost is inf, then we are fetched, we can prune that tree and we can try again with a differnet one.
+
+    while True:
+        if timer.time_out():
+            return stats
+
+        visited = set()
+
+        stack = [currentNode]
+        tour = []
+        while stack:
+            node = stack[-1]
+            if node not in visited:
+                visited.add(node)
+                currentMin = math.inf
+                nextNode = None
+                for i in range(len(edges[node])):
+                    if i != node and i not in visited:  # make sure we avoid the null
+                        if edges[node][i] < currentMin:
+                            currentMin = edges[node][i]
+                            nextNode = i
+
+                stack.pop()
+                if nextNode is not None:
+                    stack.append(nextNode)
+
+                tour.append(node)
+
+        currentNode += 1
+
+        if len(tour) == len(edges):
+            cost = score_tour(tour, edges)
+
+            if math.isinf(cost):
+                n_nodes_pruned += 1
+                cut_tree.cut(tour)
+                currentNode += 1
+                continue
+
+            if stats and cost > stats[-1].score:
+                n_nodes_pruned += 1
+                cut_tree.cut(tour)
+
+                continue
+
+            stats.append(SolutionStats(
+                tour=tour,
+                score=cost,
+                time=timer.time(),
+                max_queue_size=1,
+                n_nodes_expanded=n_nodes_expanded,
+                n_nodes_pruned=n_nodes_pruned,
+                n_leaves_covered=cut_tree.n_leaves_cut(),
+                fraction_leaves_covered=cut_tree.fraction_leaves_covered()
+            ))
+
+    if not stats:
+        return [SolutionStats(
+            [],
+            math.inf,
+            timer.time(),
+            1,
+            n_nodes_expanded,
+            n_nodes_pruned,
+            cut_tree.n_leaves_cut(),
+            cut_tree.fraction_leaves_covered()
+        )]
+    return stats
 
 
 def branch_and_bound(edges: list[list[float]], timer: Timer) -> list[SolutionStats]:
